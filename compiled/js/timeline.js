4eac540a4169a7860d6c45338dfceda0a0810d81
ali_o_kan <laurent@geturl.net>
2013-07-04 19:07:21 +0200

    test

--- compiled/js/timeline.js
+++ compiled/js/timeline.js
@@ -4561,8 +4561,14 @@ if(typeof VMM != 'undefined' && typeof VMM.MediaElement == 'undefined') {
 				} else if (m.type		==	"website") { 
 					
 					mediaElem			=	"<div class='media-shadow website' id='" + m.uid + "'>" + loading_messege + "</div>";
-                    if(data.media.asset != null && data.media.asset.screenshot != null && data.media.asset.screenshot != "") m.screenshot = data.media.asset.screenshot;
-				
+                    trace(data.media.asset.screenshot);
+                    if(data.media.asset != null && data.media.asset.screenshot != null && data.media.asset.screenshot != "") 
+                    {
+                        trace(m);
+                        trace(data.media.asset.screenshot);
+                        m.screenshot = data.media.asset.screenshot;
+                        trace(m);
+                    }
 					VMM.ExternalAPI.webthumb.get(m);
 					//mediaElem			=	"<div class='media-shadow website'><a href='" + m.id + "' target='_blank'>" + "<img src='http://api1.thumbalizr.com/?url=" + m.id.replace(/[\./]$/g, "") + "&width=300' class='media-image'></a></div>";
 					
